551035b59f3a34115e3838de5cf5e1f01f8f0c31
KarinG <karin@semper-it.com>
2024-10-26 15:21:44 -0600

    adding logic

--- openinghours.php
+++ openinghours.php
@@ -30,3 +30,18 @@ function openinghours_civicrm_install(): void {
 function openinghours_civicrm_enable(): void {
   _openinghours_civix_civicrm_enable();
 }
+
+function openinghours_civicrm_buildForm($formName, &$form) {
+  if ($formName != 'CRM_Contribute_Form_Contribution') {
+    return;
+  } else {
+    \Drupal::logger('openinghours')->info(print_r($form, true));
+    // what time is it?
+    $datetime = date('m/d/Y h:i:s a', time());
+    \Drupal::logger('openinghours')->info(print_r($datetime, true));
+    // retrieve from settings - for now:
+    $opening = "10:00";
+    $closing = "15:00";
+    // determine if we're open for businnes
+  }
+}
